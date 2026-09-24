f7fe2f53715a55fa859f94017a201ca2683fda82
pereiradanielc54-blip <pereiradanielc54@gmail.com>
2026-09-24 11:25:42 -0300

    chore: Bingo Nerdora cache v32

--- bingo-nerdora/sw.js
+++ bingo-nerdora/sw.js
@@ -1,4 +1,4 @@
-const CACHE="bingo-nerdora-v31";
+const CACHE="bingo-nerdora-v32";
 const CORE=["./","./index.html","./style.css","./app.js","./extras.js","./manifest.json","./icon.svg","./assets/neon-theme.mp3"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
