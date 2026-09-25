258e95e9a335b3f23eea1571ff0fd21df2f3ac6a
pereiradanielc54-blip <pereiradanielc54@gmail.com>
2026-09-24 23:41:44 -0300

    chore: cache and version v35

--- bingo-nerdora/sw.js
+++ bingo-nerdora/sw.js
@@ -1,5 +1,5 @@
-const CACHE="bingo-nerdora-v34";
-const VERSION="34";
+const CACHE="bingo-nerdora-v35";
+const VERSION="35";
 const CORE=["./","./index.html","./style.css","./app.js","./extras.js","./manifest.json","./icon.svg","./assets/bingo-cover.webp","./assets/neon-theme.mp3"];
 
 self.addEventListener("install",event=>{
